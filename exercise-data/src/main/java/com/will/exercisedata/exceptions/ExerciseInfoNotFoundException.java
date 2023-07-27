fc37e05c36987b16b717288776ea76bbe9faffdf
WillBurdett <williamburdett1995@gmail.com>
2023-07-27 14:45:52 +0100

    exercise not found exceptions extend illegal arguments exception now

--- exercise-data/src/main/java/com/will/exercisedata/exceptions/ExerciseInfoNotFoundException.java
+++ exercise-data/src/main/java/com/will/exercisedata/exceptions/ExerciseInfoNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class ExerciseInfoNotFoundException extends IllegalStateException {
+public class ExerciseInfoNotFoundException extends IllegalArgumentException {
 
   public ExerciseInfoNotFoundException(String message){
     super(message);
